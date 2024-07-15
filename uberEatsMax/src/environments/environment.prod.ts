74eb8606caf9a44e7c4b5bc474471b56c27e6720
Straaizo <en.sabattini@duocuc.cl>
2024-07-15 02:23:07 -0400

    Netlify

--- uberEatsMax/src/environments/environment.prod.ts
+++ uberEatsMax/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig :{
+    apiKey: "AIzaSyCHpUV2u-USVLk5m0EKJxgk2-nPKcg-fJE",
+    authDomain: "ubereats-917b8.firebaseapp.com",
+    projectId: "ubereats-917b8",
+    storageBucket: "ubereats-917b8.appspot.com",
+    messagingSenderId: "230320629615",
+    appId: "1:230320629615:web:025ece82acdd322b582a9a"
+  }
 };
